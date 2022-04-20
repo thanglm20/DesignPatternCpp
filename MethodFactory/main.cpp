e249a9f80335284b5cea41e09c73bea68f684c81
thanglm20 <lemanhthang1997@gmail.com>
2022-04-20 16:51:47 +0700

    ...

---
+++ MethodFactory/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Method Factory Pattern Design" << std::endl;
+    return 0;
+}
---
+++ build/MethodFactory/CMakeFiles/method_factory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/method_factory.dir/main.cpp.o"
+  "method_factory"
+  "method_factory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/method_factory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
