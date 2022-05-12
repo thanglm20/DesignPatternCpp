5ee1128370025ed9e8d15d9bed625387406e0020
thanglm20 <lemanhthang1997@gmail.com>
2022-05-12 16:17:04 +0700

    ...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <string>
+#include <sstream>
 int main()
 {
-    std::cout << "Patterns Design by Le Manh Thang" << std::endl;
+    std::cout << "Patterns Design in C++" << std::endl;
+
+    std::ostringstream oss;
+    std::size_t pos = 0;
+    std::size_t prevPos = pos;
+    std::string str = "thanga";
+    pos  = str.find('a');
+    std::cout << "pos: " << pos << std::endl;
+    oss << str.substr(prevPos, pos - prevPos);
+    std::cout << "oss: " << oss.str() << std::endl;
+
     return 0;
 }
