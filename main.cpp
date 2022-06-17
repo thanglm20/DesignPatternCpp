7eda8ea96ccbed0cde9a9f4a9325b7fe9c94da15
thanglm20 <lemanhthang1997@gmail.com>
2022-06-17 18:11:29 +0700

    ...

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <tuple>
 #include <experimental/tuple>
 #include <string>
-#include "function.hpp"
 
 using namespace std;
 
@@ -13,13 +12,13 @@ void print()
 template <typename T, typename... Types>
 void print(T var1, Types... var2)
 {
-    cout << var1 << endl;
+    cout << var1 ;
  
     print(var2...);
 }
 
 template<typename T>
-T first(T& number)
+T add(T& number)
 {
     return number;
 }
@@ -29,10 +28,9 @@ T add(T f, Args... args)
     return f + add(args...);
 }
 int main() {
-    int result1 = add(4, 5);
-    float result2 = add(5.0, 5.0);
-    std::cout << result1<< std::endl;
-    std::cout << result2 << std::endl;
+
+    print("[INFO] ",
+    "Variadic template function in C++ ", "\n\tby ThangLM ", 1997 , "\n");
     std::cout << "sum: " << add(1, 2, 3) << std::endl;
     return 0;
 }
