2afaf0d3059283e9cae6a50ece3bcc6df1e78b2f
thanglm20 <lemanhthang1997@gmail.com>
2022-06-17 18:05:09 +0700

    ...

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
-#include <vector>
-#include <memory>
+#include <tuple>
+#include <experimental/tuple>
 #include <string>
-#include <sstream>
-int main()
+#include "function.hpp"
+
+using namespace std;
+
+void print()
 {
-    std::cout << "Patterns Design in C++" << std::endl;
+
+}
+template <typename T, typename... Types>
+void print(T var1, Types... var2)
+{
+    cout << var1 << endl;
+ 
+    print(var2...);
+}
+
+template<typename T>
+T first(T& number)
+{
+    return number;
+}
+template <typename T, typename... Args>
+T add(T f, Args... args)
+{
+    return f + add(args...);
+}
+int main() {
+    int result1 = add(4, 5);
+    float result2 = add(5.0, 5.0);
+    std::cout << result1<< std::endl;
+    std::cout << result2 << std::endl;
+    std::cout << "sum: " << add(1, 2, 3) << std::endl;
     return 0;
 }
