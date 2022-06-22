d54350982836079b3d9ca91af402117f5f6aa31c
thanglm20 <lemanhthang1997@gmail.com>
2022-06-22 08:31:08 +0700

    ...

---
+++ Behavioral/Iterator/main.cpp
@@ -0,0 +1,80 @@
+
+
+/*
+defines an interface for accessing and traversing elements
+*/
+
+#include <iostream>
+#include <vector>
+
+template <class T>
+class Iterator
+{
+public:
+    virtual bool hasNext() = 0;
+    virtual T next() = 0;
+};
+
+template <class T>
+class Container
+{
+public:
+    Iterator<T> *getIterator()
+    {
+        return new IIterator(this);
+    }
+    void add(T item)
+    {
+        m_vec.push_back(item);
+    }
+
+private:
+    std::vector<T> m_vec;
+
+    // nested class that can aaccess any member of enclosing class
+    class IIterator : public Iterator<T> 
+    {
+
+    private:
+        int index = 0;
+        Container *m_con;
+
+    public:
+        IIterator(Container *con) : m_con(con) {}
+        bool hasNext() override
+        {
+
+            if (index < m_con->m_vec.size())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        T next() override
+        {
+            if (hasNext())
+            {
+                T a = m_con->m_vec[index++];
+                return a;
+            }
+        }
+    };
+};
+
+int main()
+{
+    Container<int> container;
+    container.add(1);
+    container.add(2);
+    container.add(3);
+    container.add(4);
+    container.add(5);
+    for (auto it = container.getIterator(); it->hasNext();)
+    {
+        int item = it->next();
+        std::cout << item << std::endl;
+    }
+
+    return 0;
+}
