4d89a932e1ad7a5e8442d6e0bd441facb63f896a
thanglm20 <lemanhthang1997@gmail.com>
2022-07-15 09:13:02 +0700

    ...

--- README.md
+++ README.md
@@ -6,4 +6,21 @@ mkdir build
 cd build
 cmake ..
 make
+
 ```
+## Run
+./Creational/Singleton/singleton
+./Creational/FactoryMethod/factory_method
+./Creational/Builder/builder
+./Behavioral/Observer/observer
+./Behavioral/Strategy/strategy
+./Behavioral/TemplateMethod/template_method
+./Behavioral/Iterator/iterator
+./Behavioral/Command/command
+./Behavioral/Interpreter/interpreter
+./Behavioral/Visitor/visitor
+./Structural/Adapter/adapter
+./Structural/Facade/facade
+./Structural/Proxy/proxy
+./Structural/Composite/composite
+./MultiThread/multi_thread
--- Structural/Facade/main.cpp
+++ Structural/Facade/main.cpp
@@ -1,35 +1,88 @@
+
 #include <iostream>
 
-class Scanner {
- public:
-  void Scan() { std::cout << "generate token\n"; }
-};
+/*
+  Façade
+  Provide a unified interface to a set of interfaces in a subsystem. 
+  Facade defines a higher-level interface that makes the subsystem easier to use. The pattern has structural purpose and applies to objects.
 
-class Parser {
- public:
-  void Parse() { std::cout << "build syntax tree\n"; }
-};
-
-class CodeGenerator {
- public:
-  void GenerateCode() { std::cout << "generate machine code\n"; }
-};
-
-class Compiler {
- public:
-  void Run() {
-    Scanner s;
-    s.Scan();
-
-    Parser p;
-    p.Parse();
-
-    CodeGenerator c;
-    c.GenerateCode();
+  When to use
+  you want to provide a simple interface to a complex subsystem
+  there are many dependencies between clients and the implementation classes of an abstraction
+  you want to layer your subsystems, use a facade to define an entry point to each subsystem level
+*/
+class SubsystemA
+{
+public:
+  void suboperation()
+  {
+    std::cout << "Subsystem A method" << std::endl;
+    // ...
   }
+  // ...
 };
 
-int main() {
-  Compiler compiler;
-  compiler.Run();
+class SubsystemB
+{
+public:
+  void suboperation()
+  {
+    std::cout << "Subsystem B method" << std::endl;
+    // ...
+  }
+  // ...
+};
+
+class SubsystemC
+{
+public:
+  void suboperation()
+  {
+    std::cout << "Subsystem C method" << std::endl;
+    // ...
+  }
+  // ...
+};
+
+/*
+* Facade
+* delegates client requests to appropriate subsystem object
+* and unified interface that is easier to use
+*/
+class Facade
+{
+public:
+  Facade() : subsystemA(), subsystemB(), subsystemC() {}
+  
+  void operation1()
+  {
+    subsystemA->suboperation();
+    subsystemB->suboperation();
+    // ...
+  }
+  
+  void operation2()
+  {
+    subsystemC->suboperation();
+    // ...
+  }
+  // ...
+  
+private:
+  SubsystemA *subsystemA;
+  SubsystemB *subsystemB;
+  SubsystemC *subsystemC;
+  // ...
+};
+
+
+int main()
+{
+  Facade *facade = new Facade();
+  
+  facade->operation1();
+  facade->operation2();
+  delete facade;
+  
+  return 0;
 }
