2b28bcc5ca0fe18458611c14c05d73bb375852ba
thanglm20 <lemanhthang1997@gmail.com>
2023-04-13 11:08:55 +0700

    add idioms

---
+++ Idioms/Piml.cpp
@@ -0,0 +1,45 @@
+ /*
+Pointer to implementation using the PImpl Idiom, which hides the implementation in the headers and includes an interface file that compiles instantly.
+Using hazard pointer or opaque pointer to implement
+An opaque pointer is a pointer that points to a data structure whose contents are not exposed at the time of its definition. 
+The following pointer is opaque. One can’t know the data contained in STest structure by looking at the definition.
+struct STest* pSTest;
+
+*/
+
+#include <iostream>
+#include <memory>
+
+// header file
+class Executor{
+public:
+    explicit Executor();
+    ~Executor();
+    void execute();
+private:
+    class Piml;
+    std::unique_ptr<Piml> m_piml;
+};
+
+// source file, any change in Piml will not affect to user or client code.
+class Executor::Piml{
+public:
+    explicit Piml(){}
+    ~Piml(){}
+    void implemetation(){
+        std::cout << "Implement and change somthing here\n";
+    }
+};
+
+Executor::Executor(){}
+Executor::~Executor(){}
+void Executor::execute(){
+    m_piml->implemetation();
+}
+
+int main(){
+    
+    Executor exe;
+    exe.execute();
+    return 0;
+}
---
+++ Idioms/SFINAE.cpp
@@ -0,0 +1,53 @@
+/*
+SFINAE (Substitution Failure Is Not An Error) is a powerful C++ language feature that allows for compile-time checking of template parameters. 
+SFINAE is a technique used to selectively enable or disable templates based on the properties of the types that are being passed as template arguments.
+
+*/
+#include <type_traits>
+#include <new>
+#include <iostream>
+#include <string>
+template <typename T>
+struct has_member_foo
+{
+    // Check if T has a member function called "foo"
+    template <typename U>
+    static decltype(std::declval<U>().foo(), std::true_type()) check(int);
+
+    // SFINAE: if "foo" doesn't exist, this function is discarded from the overload set
+    template <typename U>
+    static std::false_type check(...);
+
+    // A value of "true" indicates that T has a member function called "foo"
+    static constexpr bool value = decltype(check<T>(0))::value;
+};
+
+template <typename T>
+typename std::enable_if<has_member_foo<T>::value, void>::type
+call_foo(T& obj)
+{
+    obj.foo();
+}
+
+// Example usage:
+struct MyType {
+    void foo() { std::cout << "MyType::foo()" << std::endl; }
+};
+
+template <typename T, 
+typename std::enable_if_t<std::is_integral<T>::value, int> = true>
+T sum(T a, T b){
+    return (a + b);
+}
+
+int main()
+{
+    MyType obj;
+    call_foo(obj); // This will compile and call obj.foo()
+    
+    int i = 42;
+    // call_foo(i);   // This will not compile, because int has no member function called "foo"
+    sum(1, 2);
+    // sum(1, 0.0);// compile error
+    return 0;
+}
